7218684f80c01d8b3e97793af383203371879915
Timothy Marcoe <Timothy Marcoe@Timothy>
2016-05-22 21:04:55 +0800

    Added database support for coupons Added database support for extra inventory info like size and color, as well as support for different types of delivery methods such as downloading or printing.

---
+++ Gemma/src/com/gemma/web/dao/Coupon.java
@@ -0,0 +1,46 @@
+package com.gemma.web.dao;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Coupon {
+	
+	@Id
+	private String couponId;
+	private String name;
+	private String description;
+	private boolean active;
+	private String ruleName;
+	
+	public String getCouponId() {
+		return couponId;
+	}
+	public void setCouponId(String couponId) {
+		this.couponId = couponId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public boolean isActive() {
+		return active;
+	}
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	public String getRuleName() {
+		return ruleName;
+	}
+	public void setRuleName(String ruleName) {
+		this.ruleName = ruleName;
+	}
+}
